04b62cab3ab933fd232e680080c95067207204c5
puppitty <33668867+puppitty@users.noreply.github.com>
2018-04-09 16:22:50 -0400

    Twitter working

--- liri.js
+++ liri.js
@@ -1,72 +1,44 @@
 require("dotenv").config();
 
 // add code to require keys.js and save as a variable
-var Keys = require("./keys.js");
+var keys = require("./keys.js");
 
+// var spotify = require('spotify');
+// Declare variable from input
+var input = (process.argv[2]);
+
+// String this to include spaces
+var songMovie = (process.argv[3]);
+
+// need to capture argv[3] ignoring spaces for Spotify and Movie
+
+// Twitter
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitter);
 
 // Sample Twitter code
 
-var Twitter = require('twitter');
+if (input === "my-tweets") {
+// Move this to function and call function
+var params = {screen_name: 'Joyce4Raritan', count: 10};
+ client.get('statuses/user_timeline', params, function(error, tweets, response) {
  
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
-});
- 
-var params = {Joyce4Raritan: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+   if (!error) {
+     for (var tweet in tweets)
+     {
+       console.log(tweets[tweet].text);
+     }
+ }});
+} else if (input === "spotify-this-song") {
+// Query the Spotify database with songMovie, display artist(s), song name, Spotify preview link, album
 
-client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
-  if(error) throw error;
-  console.log(tweet);  // Tweet body. 
-  console.log(response);  // Raw response object.
-
-  client.get('favorites/list', function(error, tweets, response) {
-    if(error) throw error;
-    console.log(tweets);  // The favorites. 
-    console.log(response);  // Raw response object. 
-  });
-
-
+}
+else if (input === "movie-this") {
+  // Query the imdb database with songMovie, display movie title, year, IMDB rating Rotten Tomatoes rating, country, plot, actors
   
-// access your keys like this with "new" equal to variable name above
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-});
-
-// Directions:
-// my-tweets
-
-// spotify-this-song
-
-// movie-this
-
-// do-what-it-says
-
-// Spotify
-var spotify = require('spotify');
- 
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
- 
-    // Do something with 'data' 
-});
-
-// lookup: 
-// function({ type: 'artist OR album OR track', id: 'Spotify ID Hash' }, hollaback);
-
-// search: 
-// function({ type: 'artist OR album OR track', query: 'My search query' }, hollaback);
-
-// get: 
-// function(query, hollaback) 
-// See http://developer.spotify.com/en/metadata-api/overview/; 
+  }
+   
+else if (input === "do-what-it-says") {
+  // use fs node package and run the song listed in random.txt
+  
+  }
