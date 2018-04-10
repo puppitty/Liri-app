50ff5a6f23573e5db2846b1410d26c267ce97c15
puppitty <33668867+puppitty@users.noreply.github.com>
2018-04-10 17:54:32 -0400

    More working including movie, but not song

--- liri.js
+++ liri.js
@@ -8,7 +8,8 @@ var keys = require("./keys.js");
 var request = require("request");
 
 // var spotify = require('spotify');
-var SpotifyWebApi = require('spotify-web-api-node');
+var Spotify = require('spotify-web-api-node');
+var spotify = new Spotify(keys.spotify)
 
 // Require Twitter
 var Twitter = require('twitter');
@@ -71,7 +72,7 @@ function myTweets() {
   };
   client.get('statuses/user_timeline', params, function (error, tweets, response) {
     if (!error) {
-      for(var i = 0; i<tweets.length; i++){
+      for (var i = 0; i < tweets.length; i++) {
         var date = tweets[i].created_at;
         console.log("Joyce4Raritan: " + tweets[i].text + "\nCreated At: " + date.substring(0, 19));
         console.log("-----------------------");
@@ -79,84 +80,78 @@ function myTweets() {
     } else {
       console.log("Error occurred");
     }
+
   });
+}
 
-  // Query the Spotify database with songMovie, display artist(s), song name, Spotify preview link, album
-
-  // Search tracks whose name, album or artist contains 'Love'
-  // Spotify Function
-  function spotifySong(songMovie) {
-
-    // spotifyApi.searchTracks('songMovie')
-    //   .then(function (data) {
-    //     console.log('Search by: ' + songMovie, data.body);
-    //   }, function (err) {
-    //     console.error(err);
-    //   });
-
-    spotifyApi.searchTracks({
-      query: songMovie
-    }, function (error, data) {
-      if (!error) {
-        for (var i = 0; i < data.tracks.items.length; i++) {
-          var songData = data.tracks.items[i];
-          //artist
-          console.log("Artist: " + songData.artists[0].name);
-          //song name
-          console.log("Song: " + songData.name);
-          //spotify preview link
-          console.log("Preview URL: " + songData.preview_url);
-          //album name
-          console.log("Album: " + songData.album.name);
-          console.log("-----------------------");
-        }
-      } else {
-        console.log("Error occurred.");
+// Spotify Function
+// Query the Spotify database with songMovie, display artist(s), song name, Spotify preview link, album
+function spotifySong(songMovie) {
+  spotify.searchTracks({
+    type: "tracks",
+    query: songMovie
+  }, function (error, data) {
+    if (!error) {
+      console.log(songMovie);
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var songData = data.tracks.items[i];
+        //artist
+        console.log("Artist: " + songData.artists[0].name);
+        //song name
+        console.log("Song: " + songData.name);
+        //spotify preview link
+        console.log("Preview URL: " + songData.preview_url);
+        //album name
+        console.log("Album: " + songData.album.name);
+        console.log("-----------------------");
       }
-    });
-
-    // Query the imdb database with songMovie, display movie title, year, IMDB rating Rotten Tomatoes rating, country, plot, actors
-    function movieOmdb(songMovie) {
-      var omdbURL = 'http://www.omdbapi.com/?t=' + songMovie + '&plot=short&tomatoes=true&apikey=trilogy';
-
-      request(omdbURL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var body = JSON.parse(body);
-
-          console.log("Title: " + body.Title);
-          console.log("Release Year: " + body.Year);
-          console.log("IMdB Rating: " + body.imdbRating);
-          console.log("Country: " + body.Country);
-          console.log("Language: " + body.Language);
-          console.log("Plot: " + body.Plot);
-          console.log("Actors: " + body.Actors);
-          console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-          console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
-          //adds text to log.txt
-          fs.appendFile('log.txt', "Title: " + body.Title);
-          fs.appendFile('log.txt', "Release Year: " + body.Year);
-          fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-          fs.appendFile('log.txt', "Country: " + body.Country);
-          fs.appendFile('log.txt', "Language: " + body.Language);
-          fs.appendFile('log.txt', "Plot: " + body.Plot);
-          fs.appendFile('log.txt', "Actors: " + body.Actors);
-          fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-          fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
-
-        } else {
-          console.log('Error occurred.')
-        }
-        if (songMovie === "Mr. Nobody") {
-          console.log("-----------------------");
-          console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-          console.log("It's on Netflix!");
-
-          // Then run a request to the OMDB API with the movie specified
-        }
-        // use fs node package and run the song listed in random.txt
-
-      })
+    } else {
+      console.log("Error occurred.");
     }
-  }
+  })
+}
+
+
+// Query the imdb database with songMovie, display movie title, year, IMDB rating Rotten Tomatoes rating, country, plot, actors
+function movieOmdb(songMovie) {
+  var omdbURL = 'http://www.omdbapi.com/?t=' + songMovie + '&plot=short&tomatoes=true&apikey=trilogy';
+
+  request(omdbURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      // //adds text to log.txt
+      // fs.appendFile('log.txt', "Title: " + body.Title);
+      // fs.appendFile('log.txt', "Release Year: " + body.Year);
+      // fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
+      // fs.appendFile('log.txt', "Country: " + body.Country);
+      // fs.appendFile('log.txt', "Language: " + body.Language);
+      // fs.appendFile('log.txt', "Plot: " + body.Plot);
+      // fs.appendFile('log.txt', "Actors: " + body.Actors);
+      // fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
+      // fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
+
+    } else {
+      console.log('Error occurred.')
+    }
+    if (songMovie === "Mr. Nobody") {
+      console.log("-----------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      // Then run a request to the OMDB API with the movie specified
+    }
+    // use fs node package and run the song listed in random.txt
+
+  })
 }
